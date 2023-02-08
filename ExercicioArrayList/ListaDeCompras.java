1d36361907d9d6b05c5309610b290f8bb213a235
Marcos Andrade <marcosp.360l@gmail.com>
2023-02-08 19:35:20 -0300

    Lista de Compras

---
+++ ExercicioArrayList/ListaDeCompras.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ListaDeCompras {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList <String> Lista = new ArrayList<>();
+		
+		System.out.println("Quantos itens serão adicionados?");
+		int itens = sc.nextInt();
+		
+		System.out.println("Digite os itens: ");
+		for (int i=01;i<=itens;i++) {
+			Lista.add(sc.nextLine());
+		}
+		
+		System.out.println("Lista de compras: ");
+		for(String Total:Lista) {			
+			System.out.println(Total);
+		}
+	}
+
+}
